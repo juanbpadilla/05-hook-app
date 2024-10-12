90b669f59785373cec170284d4e42272bee34210
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-12 02:34:46 -0400

    useCounter - CustomHook
    Add CounterWithCustomHook component using useState hook

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 // import { HooksApp } from './HooksApp'
-import { CounterApp } from './01-useState/CounterApp'
+// import { CounterApp } from './01-useState/CounterApp'
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </StrictMode>,
 )
