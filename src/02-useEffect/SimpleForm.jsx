8b159dae4d4339b93ca28d047b144799f3a7241b
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-12 04:20:38 -0400

    Propiedades calculadas - useEffect - SimpleForm
    Añadir componente SimpleForm usando useState y useEffect | Multiples campos con un solo manejador de eventos

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react"
+
+
+export const SimpleForm = () => {
+
+  /**
+   * useState
+   * 
+   * inicializamos el estado del formulario, con un objeto que contiene
+   * dos propiedades: username y email.
+   */
+  const [formState, setFormState] = useState({
+    username: 'strider',
+    email: 'juan@google.com'
+  })
+
+  /**
+   * Desestructuramos el objeto formState, para obtener las propiedades
+   * username y email.
+   */
+  const { username, email } = formState;
+
+  /**
+   * onInputChange es un manejador de eventos que se encarga de actualizar
+   * el estado del formulario, cada vez que el usuario escribe en los campos de texto.
+   * 
+   * Este manejador de eventos recibe un objeto event, que contiene la información del evento
+   * ocurrido. Destructuramos el objeto event, para obtener las propiedades name y value.
+   * 
+   * Luego, actualizamos el estado del formulario, utilizando el método setFormState,
+   * y pasamos un nuevo objeto que contiene las propiedades del formulario, y actualizamos
+   * la propiedad que se corresponde con el campo de texto que se está editando.
+   * 
+   * En el setState, utilizamos el operador spread para copiar las propiedades del objeto
+   * formState, y luego actualizamos la propiedad que se corresponde con el campo de texto
+   * que se está editando.
+   * 
+   * La expresión [ name ]: value es una forma de actualizar una propiedad de un objeto,
+   * sobreescribiendo la propiedad que se corresponde con el campo de texto que se está
+   * editando. Es decir, si el campo de texto que se está editando es username, entonces
+   * se actualiza la propiedad username del objeto formState, con el nuevo valor que se
+   * está escribiendo en el campo de texto. Lo mismo ocurre con el campo de texto email.
+   * Esta es una propiedad computada, que se evalúa en tiempo de ejecución.
+   * 
+   * @param {Event} event 
+   */
+  const onInputChange = ({ target }) => {
+    // console.log(event.target.name);
+    const { name, value } = target;
+    // console.log({ name, value });
+    setFormState({
+      ...formState,
+      [ name ]: value
+    })
+  }
+
+
+  useEffect( () => {
+    console.log('useEffect called!');
+  });
+
+
+  return (
+    <>
+      <h1>Formulario Simple</h1>
+      <hr />
+
+      <input
+        type="text"
+        className='form-control'
+        placeholder='Username'
+        name='username'
+        value={ username }
+        onChange={ onInputChange }
+      />
+
+      <input
+        type="email"
+        className='form-control mt-2'
+        placeholder='email@example.com'
+        name='email'
+        value={ email }
+        onChange={ onInputChange }
+      />
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+import { SimpleForm } from './02-useEffect/SimpleForm'
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CounterWithCustomHook />
+    <SimpleForm />
   </StrictMode>,
 )
