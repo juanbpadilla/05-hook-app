5ad92b92c9d9df8e34a098b7ab5003da673b5b3d
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-12 03:22:58 -0400

    Exponer métodos del Hook
    Se refactorizó el componente CounterWithCustomHook y el custom hook useCounter para mejorar la funcionalidad del contador. Se agregaron comentarios explicativos en el código para facilitar su comprensión y se corrigieron algunos errores menores.

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -1,18 +1,58 @@
 import { useCounter } from "../hooks/useCounter"
 
-
+/**
+ * CounterWithCustomHook component
+ * 
+ * Este componente muestra un contador que utiliza un custom hook para manejar el estado del contador.
+ *  
+ * @returns 
+ */
 export const CounterWithCustomHook = () => {
 
-  const { counter } = useCounter();
+  const { counter, increment, decrement, reset } = useCounter();
 
+  /**
+   * Contiene tres botones que permiten incrementar, decrementar y resetear el contador.
+   * 
+   * Tanto en el botón de incrementar como en el de decrementar, se debe pasar un argumento al método
+   * increment o decrement. Esto se debe a que en el custom hook, los métodos increment y decrement
+   * reciben un argumento. Por lo que se debe pasar un argumento al método.
+   * 
+   * El botón de incrementar si no se especifica un valor, incrementa en 1.
+   * Para incrementar en un valor específico, se debe pasar un argumento al método increment.
+   * por ejemplo: increment( 2 ). Pero la forma de hacerlo es con una arrow function, por ejemplo:
+   * onClick={ () => increment( 2 ) }. Esto se debe a que en el custom hook, el método increment
+   * recibe un argumento, por lo que se debe pasar un argumento al método.
+   * 
+   * 🚫 No se debe enviar de esta manera onClick={ increment }, ya que así estamos enviando
+   * el evento del click, y no el valor de incremento.
+   * 
+   * ✅ De manera predeterminada, el valor de incremento es 1. en este caso, 
+   * se debe pasar de esta manera: onClick={ () => increment() }.
+   * 
+   * En el caso del botón reset, no se necesita pasar un argumento, ya que la función reset
+   * no recibe argumentos. Por lo que se puede pasar de esta manera: onClick={reset}.
+   * 
+   * El caso de decrementar es similar al de incrementar, se debe pasar un argumento al método decrement.
+   * Por ejemplo: onClick={ () => decrement( 2 ) }, para decrementar en 2.
+   */
   return (
     <>
       <h1>Counter With Hook: { counter }</h1>
       <hr />
 
-      <button className="btn btn-primary">+1</button>
-      <button className="btn btn-primary">Reset</button>
-      <button className="btn btn-primary">-1</button>
+      <button 
+        className="btn btn-primary"
+        onClick={ () => increment() }
+      >+1</button>
+      <button 
+        className="btn btn-primary"
+        onClick={reset}
+      >Reset</button>
+      <button 
+        className="btn btn-primary"
+        onClick={ () => decrement() }
+      >-1</button>
     </>
   )
 }
--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,12 +1,68 @@
 import { useState } from "react"
 
-
+/**
+ * Custom Hook useCounter
+ * 
+ * Este custom hook maneja el estado de un contador y provee funciones 
+ * para incrementar, decrementar y resetear el contador.
+ * 
+ * Tanto el valor inicial como el valor de incremento/decremento son opcionales.
+ * 
+ * Para pasar un valor inicial, se debe pasar un argumento al custom hook, por ejemplo:
+ * useCounter( 10 )
+ * 
+ * @param {*} initialValue 
+ * @returns 
+ */
 export const useCounter = ( initialValue = 10 ) => {
 
   const [counter, setCounter] = useState( initialValue )
 
+  /**
+   * Incrementa el contador en el valor especificado.
+   * 
+   * Para incrementar el contador en un valor específico,
+   * se debe pasar un argumento al método, por ejemplo:
+   * increment( 2 )
+   * 
+   * Si no se especifica un valor, se incrementa en 1.
+   * 
+   * @param {*} value 
+   * @returns 
+   */
+  const increment = ( value = 1 ) => setCounter( counter + value );
+
+  /**
+   * Decrementa el contador en el valor especificado.
+   * 
+   * Antes de decrementar el contador, se verifica que el contador no pueda decrementar
+   * por debajo de 0.
+   * 
+   * Para decrementar el contador en un valor específico,
+   * se debe pasar un argumento al método, por ejemplo:
+   * decrement( 2 )
+   * 
+   * Si no se especifica un valor, se decrementa en 1.
+   * 
+   * @param {*} value 
+   * @returns 
+   */
+  const decrement = ( value = 1 ) => {
+    if ( counter < 1 ) return;
+    setCounter( counter - value )
+  };
+
+  const reset = () => setCounter ( initialValue );
+
+  /**
+   * Se retorna un objeto con las propiedades counter, increment, decrement y reset, 
+   * para poder utilizarlas en el componente que utilice este custom hook.
+   */
   return {
     counter,
+    increment,
+    decrement,
+    reset
   }
 
 }
