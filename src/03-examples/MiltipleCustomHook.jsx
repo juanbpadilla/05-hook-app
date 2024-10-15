61a02be35d624f885048d2fde1c837ac27e15300
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-15 05:11:14 -0400

    Parametrizar y consumir nuestro custom Hook
    - Se refactorizó el uso de la función useFetch en el componente MiltipleCustomHook.jsx para obtener información de un Pokémon específico.
    - Se agregó un estado de carga para mostrar un mensaje de "Cargando..." mientras se obtiene la información.
    - Se deshabilitó la regla de linting para la variable no utilizada.
    - Se agregó un título y un encabezado para mostrar la información del Pokémon obtenida.

--- src/03-examples/MiltipleCustomHook.jsx
+++ src/03-examples/MiltipleCustomHook.jsx
@@ -3,9 +3,19 @@ import { useFetch } from "../hooks"
 
 export const MiltipleCustomHook = () => {
 
-  useFetch();
+  // eslint-disable-next-line no-unused-vars
+  const { data, hasError, isLoading } = useFetch('https://pokeapi.co/api/v2/pokemon/4');
 
   return (
-    <div>MiltipleCustomHook</div>
+    <>
+      <h1>Información de Pokémon</h1>
+      <hr />
+
+      { isLoading && <p>Cargando...</p> }
+
+      {/* <pre>{ JSON.stringify( data, null, 2 ) }</pre> */}
+
+      <h2>{ data?.name }</h2>
+    </>
   )
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react"
 
 
-export const useFetch = () => {
+export const useFetch = ( url ) => {
 
   const [state, setState] = useState({
     data: null,
@@ -14,15 +14,49 @@ export const useFetch = () => {
   useEffect(() => {
     getFetch();
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [url]);
 
-  }, [])
+  const setLoadingState = () => {
+    setState({
+      data: null,
+      isLoading: true,
+      hasError: false,
+      error: null
+    });
+  }
   
   const getFetch = async () => {
 
-    const resp = await fetch('https://pokeapi.co/api/v2/pokemon/1');
-    const data = await resp.json();
+    setLoadingState();
 
-    console.log({data});
+    const resp = await fetch( url );
+
+    // sleep
+    await new Promise( resolve => setTimeout( resolve, 1500 ) );
+
+    if ( !resp.ok ) {
+      setState({
+        data: null,
+        isLoading: false,
+        hasError: true,
+        error: {
+          code: resp.status,
+          message: resp.statusText
+        }
+      });
+      return;
+    }
+
+    const data = await resp.json();
+    setState({
+      data,
+      isLoading: false,
+      hasError: false,
+      error: null
+    });
+
+    // Manejo del cache
 
   }
   
