2fd4bf175cf9fc3ca9c4bb7074db2405047d87e5
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-16 02:33:41 -0400

    useFetch + useCounter
    Se agregó el customHook a MultipleCustomHook y se implimentó el Fetching dinámico basado en el valor del contador. Además se agregó el componente LoaddingMessage y el componente PokemonCard

--- src/03-examples/MiltipleCustomHook.jsx
+++ src/03-examples/MiltipleCustomHook.jsx
@@ -1,21 +1,48 @@
-import { useFetch } from "../hooks"
+import { useCounter, useFetch } from "../hooks"
+import { LoadingMessage } from "./LoadingMessage";
+import { PokemonCard } from "./PokemonCard";
 
 
 export const MiltipleCustomHook = () => {
 
+  const { counter, decrement, increment } = useCounter(1);
   // eslint-disable-next-line no-unused-vars
-  const { data, hasError, isLoading } = useFetch('https://pokeapi.co/api/v2/pokemon/4');
+  const { data, hasError, isLoading } = useFetch(`https://pokeapi.co/api/v2/pokemon/${ counter }`);
 
   return (
     <>
       <h1>Información de Pokémon</h1>
       <hr />
 
-      { isLoading && <p>Cargando...</p> }
+      { 
+        isLoading 
+        ? <LoadingMessage /> 
+        : <PokemonCard
+            id={ data.id }
+            name={ data.name }
+            sprites={[
+              data.sprites.front_default,
+              data.sprites.front_shiny,
+              data.sprites.back_default,
+              data.sprites.back_shiny,
+            ]} 
+          />
+      }
 
       {/* <pre>{ JSON.stringify( data, null, 2 ) }</pre> */}
 
-      <h2>{ data?.name }</h2>
+      <button 
+        className="btn btn-primary mt-2"
+        onClick={ () => counter > 1 ? decrement() : null }
+        >
+        Anterior
+      </button>
+      <button 
+        className="btn btn-primary mt-2"
+        onClick={ () => increment() }
+      >
+        Siguiente
+      </button>
     </>
   )
 }
