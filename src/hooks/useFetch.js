85cc24c2b1eb7a34b12376a455bb0e960ba9a519
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-16 03:57:43 -0400

    Incorporar caché
    Refactorizar useFetch para mejorar rendimiento y agregar caché local

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
+const localCache = {};
 
 export const useFetch = ( url ) => {
 
@@ -17,6 +17,11 @@ export const useFetch = ( url ) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [url]);
 
+  /**
+   * Esta función se encarga de actualizar el estado del componente 
+   * en un estado de carga. Se llama antes de realizar la petición fetch 
+   * para indicar que los datos se están cargando.
+   */
   const setLoadingState = () => {
     setState({
       data: null,
@@ -26,7 +31,33 @@ export const useFetch = ( url ) => {
     });
   }
   
+  /**
+   * Esta función asíncrona se encarga de realizar la petición fetch
+   * a la URL indicada en el hook useFetch.
+   * 
+   * Si la petición es exitosa, se actualiza el estado del componente
+   * con la data obtenida, y se almacena en el cache local.
+   * 
+   * Si la petición falla, se actualiza el estado del componente
+   * con un error y se muestra el mensaje de error.
+   */
   const getFetch = async () => {
+
+    {/* 
+      Esta sección verifica si la URL ya fue consultada previamente y si los datos están almacenados en el cache local.
+      Si es así, se utilizan los datos de la caché y se actualiza el 
+      estado del componente sin realizar una nueva solicitud de red.
+    */}
+    if ( localCache[url] ) {
+      console.log('Usando cache')
+      setState({
+        data: localCache[url],
+        isLoading: false,
+        hasError: false,
+        error: null
+      })
+      return;
+    }
 
     setLoadingState();
 
@@ -57,6 +88,7 @@ export const useFetch = ( url ) => {
     });
 
     // Manejo del cache
+    localCache[url] = data;
 
   }
   
