48b20b498d32a2d2684aa27291ef69ec73303427
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-11-01 06:38:25 -0400

    NavLink
    Actualizar Navbar para usar NavLink y mejorar la navegación

--- src/09-useContext/Navbar.jsx
+++ src/09-useContext/Navbar.jsx
@@ -1,12 +1,33 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 
 export const Navbar = () => {
-  return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/login">Login</Link>
-    </>
+  return (    
+    <nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary rounded-3" data-bs-theme="dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">useContext</Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+
+            {/* Podemos activar el link manualmente cuando corresponda a la ruta, pero ya no es necesario, ahora react lo hace por nosotros */}
+            <NavLink 
+              className={ ({ isActive }) => `nav-link ${ isActive ? 'active' : '' }` }
+              to="/">
+              Home
+            </NavLink>
+            <NavLink 
+              className="nav-link" 
+              to="/about">
+              About
+            </NavLink>
+            <NavLink 
+              className="nav-link" 
+              to="/login">
+              Login
+            </NavLink>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
